2242094ba9cdaa3fb5325843410c72ddf35f8647
Zohre <mohammadizohre1371@gmail.com>
2024-12-28 14:22:39 +0330

    refactor courseRegister method in CourseController

--- src/main/java/org/example/institutemanagement/controller/CourseController.java
+++ src/main/java/org/example/institutemanagement/controller/CourseController.java
@@ -8,7 +8,10 @@ import org.example.institutemanagement.dto.ResponseCourseDto;
 import org.example.institutemanagement.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,7 +24,9 @@ public class CourseController {
 
     @PostMapping("/CourseRegister")
     public ResponseEntity<Void> courseRegister(@RequestBody @Valid RegisterCourseDto dto) {
-
+        if (dto.endHour() <= dto.startHour()) {
+            throw new IllegalStateException("End hour must be greater than start hour");
+        }
         courseService.save(dto);
         return new ResponseEntity<>(HttpStatus.CREATED);
 
@@ -29,7 +34,7 @@ public class CourseController {
 
     @PostMapping("/findCourses")
     public ResponseEntity<List<ResponseCourseDto>> findCourses
-            (@RequestBody @Valid RequestCourseDto dto){
+            (@RequestBody @Valid RequestCourseDto dto) {
 
         List<ResponseCourseDto> courses = courseService.findCourses
                 (dto.teacherId(), dto.termId());
